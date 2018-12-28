b76396643e48e712f200b80b9c2d1b0e3f812d3e
544430497@qq.com <544430497@qq.com>
2018-12-28 10:37:16 +0800

    new iceclam

---
+++ test/test/loader_assamblyScript_test.ts
@@ -0,0 +1,14 @@
+import * as fs from "fs";
+import "mocha";
+import { expect } from "chai";
+const compiled = new WebAssembly.Module(fs.readFileSync("../../iceclam/build//test/untouched.wasm"));
+const imports = {};
+Object.defineProperty(module, "exports", {
+  get: () => new WebAssembly.Instance(compiled, imports).exports
+});
+
+describe("loader_assemblyScript_test", () => {
+  it("loader_test", () => {
+    expect(module.exports.add).to.not.undefined;
+  })
+});
