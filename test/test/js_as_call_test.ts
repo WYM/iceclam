0d24c97810e2c78701dcf7ab06068fc840497a71
544430497@qq.com <544430497@qq.com>
2018-12-28 11:38:54 +0800

    [修改] 测试新增mocha用例

--- test/test/js_as_call_test.ts
+++ test/test/js_as_call_test.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import "mocha";
 import { expect } from "chai";
 
-const p = path.resolve(__dirname,"../../iceclam/build/test/js_as_call_test.wasm");
+const p = path.resolve(__dirname, "../../iceclam/build/test/js_as_call_test.wasm");
 console.log(p);
 const buffer = fs.readFileSync(p);
 
@@ -16,17 +16,21 @@ function toArrayBuffer(buf) {
   return ab;
 }
 
-WebAssembly.instantiate(toArrayBuffer(buffer), {
-  env: {
-    log(s: string) {
-      expect(s).to.equals("add","why not equals add");
-      console.log(s);
-    },
-    abort(mesg, file, line, colm) {
-      throw Error("abort: " + mesg + " at " + file + ":" + line + ":" + colm);
-    }
-  }
-}).then(obj => {
-  obj.instance.exports.add(4, 2)
-}).catch(console.error);
+describe("js_as_call_test", () => {
+  it("js_as_call_test", () => {
+    WebAssembly.instantiate(toArrayBuffer(buffer), {
+      env: {
+        log(s: string) {
+          expect(s).to.equals("add", "why not equals add");
+          console.log(s);
+        },
+        abort(mesg, file, line, colm) {
+          throw Error("abort: " + mesg + " at " + file + ":" + line + ":" + colm);
+        }
+      }
+    }).then(obj => {
+      obj.instance.exports.add(4, 2)
+    }).catch(console.error);
+  })
+})
 
