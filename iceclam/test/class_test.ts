c497e8deacca0a67ecc7b46063df9d5b9baf731c
544430497@qq.com <544430497@qq.com>
2018-12-28 18:48:57 +0800

    [新增] 新增创建回收class测试

---
+++ iceclam/test/class_test.ts
@@ -0,0 +1,40 @@
+import "allocator/tlsf";
+import { Par_Class } from "./class_sub_test";
+
+// @ts-ignore
+export { memory };
+
+// @ts-ignore
+@external("env", "log")
+export declare function log(data: string): void;
+
+function alloc<T>(): T {
+    return changetype<T>(memory.allocate(offsetof<T>()));
+}
+
+function free<T>(obj: T): void {
+    memory.free(changetype<usize>(obj));
+}
+
+export function test(): void {
+    let t = alloc<Test1>();
+    t.say();
+
+    let t1 = alloc<Test2>();
+    t1.say();
+
+    free(t);
+    free(t1);
+}
+
+class Test1 extends Par_Class {
+    public say(): void {
+        log("Test");
+    }
+}
+
+class Test2 extends Par_Class {
+    public say(): void {
+        super.say();
+    }
+}
